16485ef6fd1e8d09f43dbd1f664a50728234ae2c
Laurentiu Iordache <lstoicescu@endava.com>
2018-05-03 00:50:25 +0300

    Created project sms-reader

---
+++ sms-reader/app/src/main/java/com/envision/lstoicescu/sms_reader/MainActivity.java
@@ -0,0 +1,73 @@
+package com.envision.lstoicescu.sms_reader;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu;
+        // this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { // TODO: Remove, just educational purpose
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(getApplicationContext(), "S-a apăsat item1", Toast.LENGTH_LONG).show();
+                showAlertDialogMessageClicked();
+                break;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(), "S-a apăsat item1", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(), "S-a apăsat item1", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    public void showAlertDialogMessageClicked() {
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage(getString(R.string.DIALOG_POSITIVE_RESPONSE));
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                getString(R.string.DIALOG_POSITIVE_RESPONSE),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // enter in message TODO: Can move it in caller method
+                        // read message
+                        dialog.cancel();
+                    }
+                });
+
+        builder1.setNegativeButton(
+                getString(R.string.DIALOG_NEGATIVE_RESPONSE),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // enter in message
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+}
